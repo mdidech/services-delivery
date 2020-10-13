b5c2440e6baa8feb0338d7ffa39c111393df87d4
abdel <didech.abdel@gmail.com>
2020-10-13 15:06:54 +0100

    commit color

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -4,23 +4,23 @@ import "firebase/firestore";
 import "firebase/storage";
 
 // const firebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_APIKEY}`,
-//   authDomain: `${process.env.REACT_APP_AUTHDOMAIN}`,
-//   databaseURL: `${process.env.REACT_APP_DATABASEURL}`,
-//   projectId: `${process.env.REACT_APP_PROJECTID}`,
-//   storageBucket: `${process.env.REACT_APP_STORAGEBUCKET}`,
-//   messagingSenderId: `${process.env.REACT_APP_MESSAGEINGSENDERID}`,
-//   appId: `${process.env.REACT_APP_APPID}`,
+//   apiKey: process.env.REACT_APP_APIKEY,
+  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  // databaseURL: process.env.REACT_APP_DATABASEURL,
+  // projectId: process.env.REACT_APP_PROJECTID,
+  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  // appId: process.env.REACT_APP_APPID,
 // };
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: "AIzaSyBDr-3xg6L-o1lzeMiKvZBSnz9bCJcj2hU",
+  authDomain: "lboy-services.firebaseapp.com",
+  databaseURL: "https://lboy-services.firebaseio.com",
+  projectId: "lboy-services",
+  storageBucket: "lboy-services.appspot.com",
+  messagingSenderId: "221220411603",
+  appId: "1:221220411603:web:fbf4eb23e74d100e4a1c26"
 };
 //initialize Firebase
 app.initializeApp(firebaseConfig);
